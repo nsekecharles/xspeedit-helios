dd2acc03a3bd34a86089fb3e68029d28c7029dc4
Charles NSEKE <nsekecharles@gmail.com>
2023-09-22 16:46:29 +0200

    setting up non optimized algorithm

--- src/main/java/com/nsekecharles/helios/PackagingOptimizer.java
+++ src/main/java/com/nsekecharles/helios/PackagingOptimizer.java
@@ -1,8 +1,35 @@
 package com.nsekecharles.helios;
 
+import java.util.StringJoiner;
+import java.util.stream.Stream;
+
 public class PackagingOptimizer {
+    public static final int boxSize = 10;
 
     public static String optimize(String articles) {
-        return articles;
+
+        char[] articlesNumber = articles.toCharArray();
+
+        StringJoiner joiner = new StringJoiner("/");
+
+        int currentSize = 0;
+        StringBuilder boxContent = new StringBuilder();
+
+        for (char c : articlesNumber) {
+            int parsedArticlesNumber = Character.getNumericValue(c);
+            if (currentSize + parsedArticlesNumber <= boxSize) {
+                currentSize += parsedArticlesNumber;
+                boxContent.append(parsedArticlesNumber);
+            } else {
+                joiner.add(boxContent);
+                boxContent = new StringBuilder();
+                boxContent.append(parsedArticlesNumber);
+                currentSize = parsedArticlesNumber;
+            }
+        }
+
+        joiner.add(boxContent);
+
+        return joiner.toString();
     }
 }
--- src/test/java/com/nsekecharles/helios/PackagingOptimizerTest.java
+++ src/test/java/com/nsekecharles/helios/PackagingOptimizerTest.java
@@ -30,4 +30,51 @@ class PackagingOptimizerTest {
         assertThat(actual).isEqualTo("3");
     }
 
+    @Test
+    void should_return_a_single_box_given_multiple_items_fitting_in_one_box() {
+        // Given
+        var input = "324";
+
+        // When
+        String actual = PackagingOptimizer.optimize(input);
+
+        // Assert
+        assertThat(actual).isEqualTo("324");
+    }
+
+    @Test
+    void should_return_a_two_boxes_given_multiple_items_fitting_in_two_boxes() {
+        // Given
+        var input = "3246";
+
+        // When
+        String actual = PackagingOptimizer.optimize(input);
+
+        // Assert
+        assertThat(actual).isEqualTo("324/6");
+    }
+
+    @Test
+    void should_return_a_two_boxes_given_multiple_items_fitting_in_two_boxes_2() {
+        // Given
+        var input = "3836";
+
+        // When
+        String actual = PackagingOptimizer.optimize(input);
+
+        // Assert
+        assertThat(actual).isEqualTo("3/8/36");
+    }
+
+    @Test
+    void should_return_expected_exemple() {
+        // Given
+        var input = "163841689525773";
+
+        // When
+        String actual = PackagingOptimizer.optimize(input);
+
+        // Assert
+        assertThat(actual).isEqualTo("163/8/41/6/8/9/52/5/7/73");
+    }
 }
