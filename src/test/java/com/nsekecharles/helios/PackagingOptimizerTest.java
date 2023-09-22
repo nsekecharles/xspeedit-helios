4724b020f0db1235d87d889de83cd8e9ab76194b
Charles NSEKE <nsekecharles@gmail.com>
2023-09-22 16:04:18 +0200

    rename test method

--- src/test/java/com/nsekecharles/helios/PackagingOptimizerTest.java
+++ src/test/java/com/nsekecharles/helios/PackagingOptimizerTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PackagingOptimizerTest {
 
     @Test
-    void should_empty_string_given_empty_articles_list() {
+    void should_return_empty_string_given_empty_articles_list() {
         // Given
         var input = "";
 
